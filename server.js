2da56a13a9f25443b29bce250e93f3adc936e94e
Shreyas Kanchan <kanchanshreyas@gmail.com>
2025-01-12 23:16:52 +0530

    Added vercel json

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ connectDB();
  // Serve static assets in production
  if(process.env.NODE_ENV=='production'){
     // Set Static folder
-    app.use(express.static('client/build'))
+    app.use(express.static('client/dist'))
 
     app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+        res.sendFile(path.resolve(__dirname,'client','dist','index.html'))
     })
  }
 
