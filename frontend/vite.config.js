592ddba2604ffe90b629b42b97d01cb102bd1a74
Shreyas Kanchan <kanchanshreyas@gmail.com>
2025-01-14 09:39:18 +0530

    vercel changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {   
       '/api': {
-        target: 'http://localhost:5000',
+        target: process.env.VITE_REACT_APP_BACKEND_BASEURL,
         changeOrigin: true,
         secure: false,
       },
