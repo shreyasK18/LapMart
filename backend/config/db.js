fe6a83be5d1a6bdbd943b255b20a1a05ecb22d9b
Shreyas Kanchan <kanchanshreyas@gmail.com>
2025-01-14 14:25:47 +0530

    vercel changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,7 @@
 const mongoose= require('mongoose');
 const config=require('config');
-const db='mongodb+srv://admin430:jvwi9KyLlqyotxGg@cluster0.doytz.mongodb.net/lapmart?retryWrites=true&w=majority&appName=Cluster0';//config.get('mongoURI');
+// const db='mongodb+srv://admin430:jvwi9KyLlqyotxGg@cluster0.doytz.mongodb.net/lapmart?retryWrites=true&w=majority&appName=Cluster0';//config.get('mongoURI');
+const db=process.env["NODE_CONFIG_DIR"];
 const connectDB= async()=>{
    try {
     await mongoose.connect(db,{
