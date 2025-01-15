1cfb55c498f9a9df6da18043500c9023af76e48d
Shreyas Kanchan <kanchanshreyas@gmail.com>
2025-01-15 10:30:54 +0530

    URL Changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,14 @@
-import { defineConfig,loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({mode})=>{
-
-  const env=loadEnv(mode,process.cwd(),'')
-  console.log("Environment Variable");
-  console.log(env.VITE_REACT_APP_BACKEND_BASEURL)
-  return {
-    plugins: [react()],
-    define:{
-      'process.env.PUBLIC_URL': JSON.stringify('/')
-      
+export default defineConfig({
+  plugins: [react()],
+  define:{
+    'process.env':{
+      PUBLIC_URL:'/',
     }
-  }
+    
+  },
 })
 
