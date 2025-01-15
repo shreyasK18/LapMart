fb31b9c53aace0d3b9714c97780b5cfaea0b4d67
Shreyas Kanchan <kanchanshreyas@gmail.com>
2025-01-15 12:09:37 +0530

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,22 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define:{
-    'process.env':{
-      PUBLIC_URL:'/',
-      VITE_REACT_APP_BACKEND_BASEURL:''
-    }
-    
-  },
-  server: {
-    proxy: {   
-      '/api': {
-        target: process.env.VITE_REACT_APP_BACKEND_BASEURL,
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-  
 })
 
