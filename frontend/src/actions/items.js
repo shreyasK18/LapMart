b37d671778354470fbd637bbf109d35c8a7a3714
Shreyas Kanchan <kanchanshreyas@gmail.com>
2025-01-15 09:14:18 +0530

    URL Changes

--- frontend/src/actions/items.js
+++ frontend/src/actions/items.js
@@ -4,7 +4,7 @@ import { getItemFromReducer, getItemsFromReducer,itemError } from '../reducers/i
 
 export const getItems =() =>async dispatch =>{
     try {
-        const res =await axios.get('/api/item');
+        const res =await axios.get('https://lap-mart-35z68c5ad-shreyas-kanchans-projects.vercel.app/api/item');
         
         dispatch(getItemsFromReducer(
            res.data
@@ -19,7 +19,7 @@ export const getItems =() =>async dispatch =>{
 }
 export const getItem = (id) =>async dispatch=>{
     try {
-        const res =await axios.get(`/api/item/${id}`);
+        const res =await axios.get(`https://lap-mart-35z68c5ad-shreyas-kanchans-projects.vercel.app/api/item/${id}`);
         dispatch(getItemFromReducer(res.data));
     } catch (err) {
         dispatch(itemError({
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({mode})=>{
+
   const env=loadEnv(mode,process.cwd(),'')
+  console.log("Environment Variable");
+  console.log(env.VITE_REACT_APP_BACKEND_BASEURL)
   return {
   plugins: [react()],
   define:{
