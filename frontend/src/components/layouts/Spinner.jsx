289546c85f2af31c9ee1ae3a6e87f568e842ff73
Shreyas Kanchan <kanchanshreyas@gmail.com>
2025-01-23 13:14:24 +0530

    scrooll loading issue

--- frontend/src/components/layouts/Spinner.jsx
+++ frontend/src/components/layouts/Spinner.jsx
@@ -1,6 +1,13 @@
 
-export default () => (
-
+import React,{useState} from "react";
+export default () =>{
+   const [scrollUp,setScrollUp]=useState(false);
+  if(scrollUp===false){
+    window.scrollTo(0,0);
+    setScrollUp(true);
+}
+  return (
+ 
     <div  style={{ width:"100%",height:"100%",display:"block",margin:"10% auto",padding:"10%"}} > 
       <div style={{ margin: 'auto', display: 'block' }} className="spinner-border text-dark center" >
         <span  className="sr-only">Loading...</span>
@@ -8,4 +15,4 @@ export default () => (
     </div>
    
 
-);
+)};
